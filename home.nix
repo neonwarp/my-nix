be1519dfff40bb15b762605333e705505d6dc19f
neonwarp <83647044+neonwarp@users.noreply.github.com>
2024-12-21 14:16:15 +0100

    Add rust and remove nvim

--- home.nix
+++ home.nix
@@ -1,160 +1,74 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
 
 {
-  home.username = "user";
-  home.homeDirectory = "/home/user";
+  home = rec {
+    username = "user";
+    homeDirectory = "/home/${username}";
 
-  home.stateVersion = "24.11";
+    stateVersion = "24.11";
 
-  home.packages = [
-    pkgs.volta
-    pkgs.go
-    pkgs.deno
-    pkgs.zoxide
-    pkgs.fzf
-    pkgs.neovim
-    pkgs.starship
-  ];
+    packages = [
+      pkgs.volta
+      pkgs.go
+      pkgs.deno
+      pkgs.zoxide
+      pkgs.fzf
+      pkgs.ripgrep
+      pkgs.starship
+      pkgs.rustup
+      pkgs.gcc
+      pkgs.pkg-config
+      pkgs.openssl
+    ];
 
-  home.file = {
-  };
-
-  home.sessionVariables = {
-    EDITOR = "neovim";
-  };
-
-  programs.home-manager.enable = true;
-
-  programs.zoxide = {
-    enable = true;
-    enableBashIntegration = true;
-  };
-
-  programs.git = {
-    enable = true;
-    userName = "user";
-    userEmail = "user@local";
-    aliases = {
-      a = "add --all";
-      b = "branch";
-      ls = "branch -a";
-      c = "commit -am";
-      ca = "!git add -A && git commit -am";
-      co = "checkout";
-      d = "diff --color --color-words --abbrev";
-      dt = "difftool";
-      p = "push";
-      r = "rebase";
-      s = "status -sb";
-      l = "log --decorate --all --graph";
-      h = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
+    sessionVariables = {
+      RUST_BACKTRACE = "full";
+      PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
     };
   };
 
-  programs.starship = {
-    enable = true;
-    settings = {
-      add_newline = true;
+  programs = {
+    home-manager.enable = true;
+
+    bash = {
+      enable = true;
     };
-  };
 
-  programs.neovim = {
-    extraPackages = with pkgs; [
-      lua-language-server
-      stylua
-      ripgrep
-    ];
+    zoxide = {
+      enable = true;
+      enableBashIntegration = true;
+    };
 
-    plugins = with pkgs.vimPlugins; [
-      lazy-nvim
-    ];
+    starship = {
+      enable = true;
+      settings = {
+        add_newline = true;
+      };
+    };
 
-    extraLuaConfig =
-      let
-        plugins = with pkgs.vimPlugins; [
-          # LazyVim
-          LazyVim
-          bufferline-nvim
-          cmp-buffer
-          cmp-nvim-lsp
-          cmp-path
-          cmp_luasnip
-          conform-nvim
-          dashboard-nvim
-          dressing-nvim
-          flash-nvim
-          friendly-snippets
-          gitsigns-nvim
-          indent-blankline-nvim
-          lualine-nvim
-          neo-tree-nvim
-          neoconf-nvim
-          neodev-nvim
-          noice-nvim
-          nui-nvim
-          nvim-cmp
-          nvim-lint
-          nvim-lspconfig
-          nvim-notify
-          nvim-spectre
-          nvim-treesitter
-          nvim-treesitter-context
-          nvim-treesitter-textobjects
-          nvim-ts-autotag
-          nvim-ts-context-commentstring
-          nvim-web-devicons
-          persistence-nvim
-          plenary-nvim
-          telescope-fzf-native-nvim
-          telescope-nvim
-          todo-comments-nvim
-          tokyonight-nvim
-          trouble-nvim
-          vim-illuminate
-          vim-startuptime
-          which-key-nvim
-          { name = "LuaSnip"; path = luasnip; }
-          { name = "catppuccin"; path = catppuccin-nvim; }
-          { name = "mini.ai"; path = mini-nvim; }
-          { name = "mini.bufremove"; path = mini-nvim; }
-          { name = "mini.comment"; path = mini-nvim; }
-          { name = "mini.indentscope"; path = mini-nvim; }
-          { name = "mini.pairs"; path = mini-nvim; }
-          { name = "mini.surround"; path = mini-nvim; }
-        ];
-        mkEntryFromDrv = drv:
-          if lib.isDerivation drv then
-            { name = "${lib.getName drv}"; path = drv; }
-          else
-            drv;
-        lazyPath = pkgs.linkFarm "lazy-plugins" (builtins.map mkEntryFromDrv plugins);
-      in
-      ''
-        require("lazy").setup({
-          defaults = {
-            lazy = true,
-          },
-          dev = {
-            -- reuse files from pkgs.vimPlugins.*
-            path = "${lazyPath}",
-            patterns = { "" },
-            -- fallback to download
-            fallback = true,
-          },
-          spec = {
-            { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-            -- The following configs are needed for fixing lazyvim on nix
-            -- force enable telescope-fzf-native.nvim
-            { "nvim-telescope/telescope-fzf-native.nvim", enabled = true },
-            -- disable mason.nvim, use programs.neovim.extraPackages
-            { "williamboman/mason-lspconfig.nvim", enabled = false },
-            { "williamboman/mason.nvim", enabled = false },
-            -- import/override with your plugins
-            { import = "plugins" },
-            -- treesitter handled by xdg.configFile."nvim/parser", put this line at the end of spec to clear ensure_installed
-            { "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = {} } },
-          },
-        })
-      '';
+    git = {
+      enable = true;
+      userName = "";
+      userEmail = "";
+      extraConfig = {
+        push = { autoSetupRemote = true; };
+        fetch = { rebase = true; };
+      };
+      aliases = {
+        a = "add --all";
+        b = "branch";
+        ls = "branch -a";
+        c = "commit -am";
+        ca = "!git add -A && git commit -am";
+        co = "checkout";
+        d = "diff --color --color-words --abbrev";
+        dt = "difftool";
+        p = "push";
+        r = "rebase";
+        s = "status -sb";
+        l = "log --decorate --all --graph";
+        h = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
+      };
+    };
   };
 }
